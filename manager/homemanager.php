36753278e2de85550d15e792a444dc5da8177eeb
Ika Wastiningsih <ikawastiningsih@gmail.com>
2019-01-20 19:41:43 +0700

    new

--- manager/homemanager.php
+++ manager/homemanager.php
@@ -59,7 +59,7 @@
           </a>
         </li>
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Beranda">
-          <a class="nav-link" href="home.php?konten=datakaryawan">
+          <a class="nav-link" href="homemanager.php?konten=datakaryawan">
             <i class="fa fa-fw fa-clipboard"></i>
             <span class="nav-link-text">Data Karyawan</span>
           </a>
