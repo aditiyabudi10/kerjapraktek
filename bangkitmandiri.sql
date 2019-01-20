6104b31d61f10a497adc0fa193c7abb1b0753312
Andrea Damayanti <andr.dmynti22@gmail.com>
2019-01-20 17:29:56 +0700

    new db

--- bangkitmandiri.sql
+++ bangkitmandiri.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.0.2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 30 Des 2018 pada 14.09
--- Versi Server: 10.0.17-MariaDB
--- PHP Version: 5.6.14
+-- Generation Time: Jan 20, 2019 at 11:17 AM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,100 +23,105 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `absensi`
+-- Table structure for table `absensi`
 --
 
 CREATE TABLE `absensi` (
+  `id_absensi` int(10) NOT NULL,
   `id_karyawan` int(20) NOT NULL,
-  `nama` varchar(25) NOT NULL,
-  `posisi` varchar(20) NOT NULL,
-  `penempatan` varchar(20) NOT NULL,
   `tanggal` date NOT NULL,
+  `bulan` varchar(20) NOT NULL,
   `keterangan` varchar(25) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `gaji_bulanan`
+-- Table structure for table `gaji_bulanan`
 --
 
 CREATE TABLE `gaji_bulanan` (
+  `id_gaji` int(10) NOT NULL,
   `id_karyawan` int(20) NOT NULL,
-  `nama` varchar(25) NOT NULL,
-  `posisi` varchar(20) NOT NULL,
   `bulan` varchar(15) NOT NULL,
   `tahun` int(4) NOT NULL,
-  `jumlahkehadiran` int(2) NOT NULL,
-  `totalgaji` int(10) NOT NULL
+  `gaji` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `karyawan`
+-- Table structure for table `karyawan`
 --
 
 CREATE TABLE `karyawan` (
   `id_karyawan` int(20) NOT NULL,
-  `nama` varchar(25) NOT NULL,
+  `nama_karyawan` varchar(25) NOT NULL,
   `tempat_lahir` varchar(20) NOT NULL,
   `tanggal_lahir` date NOT NULL,
   `jenis_kelamin` char(1) NOT NULL,
   `agama` varchar(25) NOT NULL,
   `alamat` varchar(30) NOT NULL,
   `posisi` varchar(20) NOT NULL,
-  `penempatan` varchar(20) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `penempatan` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `karyawan`
+-- Dumping data for table `karyawan`
 --
 
-INSERT INTO `karyawan` (`id_karyawan`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `alamat`, `posisi`, `penempatan`, `username`, `password`) VALUES
-(1, 'Aditiya Zayn Malik', 'Brebes', '1997-10-15', 'L', 'Islam', 'jl raya pulogebang', 'Admin', 'Kantor Pusat', 'admin', '21232f297a57a5a743894a0e4a801fc3'),
-(3, 'Ika', 'Bekasi', '1998-09-14', 'P', 'Islam', 'jl raya bekasi', 'Manager', 'Kantor Pusat', 'manager', '1d0258c2440a8d19e716292b231e3190');
+INSERT INTO `karyawan` (`id_karyawan`, `nama_karyawan`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `alamat`, `posisi`, `penempatan`) VALUES
+(1, 'Aditiya Zayn Malik', 'Brebes', '1997-10-15', 'L', 'Islam', 'jl raya pulogebang', 'Admin', 'Kantor Pusat'),
+(3, 'Ika', 'Bekasi', '1998-09-14', 'P', 'Islam', 'jl raya bekasi', 'Manager', 'Kantor Pusat');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `penempatan`
+-- Table structure for table `penempatan`
 --
 
 CREATE TABLE `penempatan` (
   `id_karyawan` int(20) NOT NULL,
-  `nama` varchar(25) NOT NULL,
-  `lokasi_pekerjaan` varchar(20) NOT NULL
+  `nama_tempat` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
-  `id` int(10) NOT NULL,
+  `id_user` int(10) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `nama` varchar(20) NOT NULL,
   `level` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `password`, `nama`, `level`) VALUES
-(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'aditya', 0),
-(2, 'manager', '1d0258c2440a8d19e716292b231e3190', 'Ika', 1),
-(3, 'karyawan', '9e014682c94e0f2cc834bf7348bda428', 'jordan', 2);
+INSERT INTO `user` (`id_user`, `username`, `password`, `level`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 0),
+(2, 'manager', '1d0258c2440a8d19e716292b231e3190', 1),
+(3, 'karyawan', '9e014682c94e0f2cc834bf7348bda428', 2);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `absensi`
+--
+ALTER TABLE `absensi`
+  ADD PRIMARY KEY (`id_absensi`);
+
+--
+-- Indexes for table `gaji_bulanan`
+--
+ALTER TABLE `gaji_bulanan`
+  ADD PRIMARY KEY (`id_gaji`);
 
 --
 -- Indexes for table `karyawan`
@@ -128,12 +133,22 @@ ALTER TABLE `karyawan`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `absensi`
+--
+ALTER TABLE `absensi`
+  MODIFY `id_absensi` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `gaji_bulanan`
+--
+ALTER TABLE `gaji_bulanan`
+  MODIFY `id_gaji` int(10) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `karyawan`
 --
@@ -143,7 +158,7 @@ ALTER TABLE `karyawan`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
